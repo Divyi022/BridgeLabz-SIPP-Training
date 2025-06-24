b8674390dc6e23be6fde2c3dec7bc0e0fbc368fb
Divyanshi Chauhan <156984710+Divyi022@users.noreply.github.com>
2025-06-24 13:02:29 +0530

    Delete Methods directory

--- Methods/ReverseString.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class ReverseString{
-public static void main(String[] args){
-StringBuilder  sc = new StringBuilder("Hello");
-sc.append("World");
-sc.reverse();
-System.out.println(sc);
-}
-}
